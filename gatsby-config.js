3de98f2f782927b14c82d6741addc81899fb7f07
rxnaij <richardblue37@gmail.com>
2021-03-31 13:20:53 -0400

    set up blog template page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Super Save Slot Blog`,
+    description: `Thoughts, reviews, ramblings, and whatnot about the games I've played.`,
+    author: `@rxnaij`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/pages/blog.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPage = ({ data }) => {
+    console.log(data)
+    const { nodes } = data.allMarkdownRemark
+    return (
+        <Layout>
+            <SEO title="Blog" />
+            <h1>Blog</h1>
+            <p>Here are all the posts.</p>
+            {
+                nodes ? nodes.map(({frontmatter}) => {
+                    return(
+                        <div>
+                            <h3>{frontmatter.title}</h3>
+                            <p>{frontmatter.date}</p>
+                            <p><Link to={frontmatter.path}>Read more &rarr;</Link></p>
+                        </div>
+                    )
+                })
+                : <p>No posts.</p>
+            }
+        </Layout>
+    )
+}
+
+export default BlogPage
+
+export const query = graphql`
+  {
+    allMarkdownRemark(
+      sort: {order: DESC, fields: [frontmatter___date]}
+      limit: 1000
+    ) {
+      nodes {
+        frontmatter {
+          path
+          title
+          date
+        }
+      }
+    }
+  }  
+`
