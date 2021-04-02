f48b3b9357ea3fc6820e93d5ece4c5700caafe98
rxnaij <richardblue37@gmail.com>
2021-04-01 22:36:28 -0400

    update date style

--- src/templates/BlogTemplate.js
+++ src/templates/BlogTemplate.js
@@ -2,6 +2,9 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from '../components/Layout/layout'
+import SEO from '../components/seo'
+
+import { head, date } from './BlogTemplate.module.css'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -10,15 +13,18 @@ export default function Template({
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
+      <SEO title={frontmatter.title} />
         <div className="blog-post-container">
             <div className="blog-post">
+              <div className={head}>
                 <h1>{frontmatter.title}</h1>
-                <h2>{frontmatter.date}</h2>
-                <p className="lead">{frontmatter.subtitle}</p>
-                <div
+                <p className="subtitle">{frontmatter.subtitle}</p>
+                <p className={date}>{frontmatter.date}</p>
+              </div>
+              <div
                 className="blog-post-content"
                 dangerouslySetInnerHTML={{ __html: html }}
-                />
+              />
             </div>
         </div>
     </Layout>
