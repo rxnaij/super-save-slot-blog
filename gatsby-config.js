6607f6cb58b90bcbd5b3dd1444d7f33e0b64d8f5
rxnaij <richardblue37@gmail.com>
2021-08-15 01:44:33 -0400

    update config.yml: local assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
         path: `${__dirname}/blog`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/assets`,
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
